4ae3099f48a6ed1bb47ed6c2e737bf87aa09ae74
Dongho Sim <dhsim86@gmail.com>
2018-07-07 16:23:53 +0900

    Added @NotEmpty for validation for Test Configuration.

--- src/test/java/com/dongho/dev/PropertyTestConfig.java
+++ src/test/java/com/dongho/dev/PropertyTestConfig.java
@@ -5,11 +5,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Validated
 @TestConfiguration
 @ConfigurationProperties(prefix = "fixtures")
 public class PropertyTestConfig {
 
+	@NotEmpty
 	private String test;
 }
