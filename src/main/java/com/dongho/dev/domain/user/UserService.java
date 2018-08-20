6200e1dc1d8c73a6f68ebc88868ca9c255ae4c23
Dongho Sim <dhsim86@gmail.com>
2018-08-21 02:00:34 +0900

    Added PublishOn / SubscribeOn Example

--- src/main/java/com/dongho/dev/domain/user/UserService.java
+++ src/main/java/com/dongho/dev/domain/user/UserService.java
@@ -1,4 +1,16 @@
 package com.dongho.dev.domain.user;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
 public class UserService {
+
+    public Mono<String> getUserName() {
+        return Mono.fromCallable(() -> {
+            Thread.sleep(1000);
+            log.info("{}: getUserName", Thread.currentThread().getName());
+            return "userName";
+        });
+    }
 }
--- src/main/java/com/dongho/dev/web/HelloController.java
+++ src/main/java/com/dongho/dev/web/HelloController.java
@@ -1,13 +1,23 @@
 package com.dongho.dev.web;
 
+import com.dongho.dev.domain.user.UserService;
 import com.dongho.dev.web.protocol.Message;
 import com.dongho.dev.web.protocol.StatementParameter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
+@Slf4j
 @RestController
 public class HelloController {
+
+    @Autowired
+    private UserService userService;
 
 	@GetMapping("/")
 	public Flux<Message> getMessage(StatementParameter parameter) {
@@ -16,4 +26,85 @@ public class HelloController {
 			Message.builder().body("Hello Spring Boot 2").build()
 		);
 	}
+
+    @GetMapping("/noton")
+    public Mono<String> getNoton() {
+        log.info("{}: start", Thread.currentThread().getName());
+        return Mono.fromCallable(() -> "publish")
+                .doOnNext(str -> log.info("{}: first", Thread.currentThread().getName()))
+                .flatMap(str -> Mono.defer(() -> {
+                    log.info("{}: before get user name", Thread.currentThread().getName());
+                    return userService.getUserName();
+                }))
+                .zipWith(Mono.defer(() -> Mono.fromCallable(() -> {
+                    log.info("{}: zipwith", Thread.currentThread().getName());
+                    return "zipwith";
+                })))
+                .map(tuple -> {
+                    log.info("{}: tuple map", Thread.currentThread().getName());
+                    return tuple.getT1() + tuple.getT2();
+                });
+    }
+
+	@GetMapping("/subscribeon")
+    public Mono<String> getSubsbribeOn() {
+        log.info("{}: start", Thread.currentThread().getName());
+        return Mono.fromCallable(() -> "publish")
+                .subscribeOn(Schedulers.elastic())
+                .doOnNext(str -> log.info("{}: first", Thread.currentThread().getName()))
+                .flatMap(str -> Mono.defer(() -> {
+                    log.info("{}: before get user name", Thread.currentThread().getName());
+                    return userService.getUserName();
+                }))
+                .zipWith(Mono.defer(() -> Mono.fromCallable(() -> {
+                    log.info("{}: zipwith", Thread.currentThread().getName());
+                    return "zipwith";
+                })))
+                .map(tuple -> {
+                    log.info("{}: tuple map", Thread.currentThread().getName());
+                    return tuple.getT1() + tuple.getT2();
+                });
+    }
+
+    @GetMapping("/publishon")
+    public Mono<String> getPublishOn() {
+        log.info("{}: start", Thread.currentThread().getName());
+        return Mono.fromCallable(() -> "publish")
+                .publishOn(Schedulers.elastic())
+                .doOnNext(str -> log.info("{}: first", Thread.currentThread().getName()))
+                .flatMap(str -> Mono.defer(() -> {
+                    log.info("{}: before get user name", Thread.currentThread().getName());
+                    return userService.getUserName();
+                }))
+                .zipWith(Mono.defer(() -> Mono.fromCallable(() -> {
+                    log.info("{}: zipwith", Thread.currentThread().getName());
+                    return "zipwith";
+                })))
+                .map(tuple -> {
+                    log.info("{}: tuple map", Thread.currentThread().getName());
+                    return tuple.getT1() + tuple.getT2();
+                });
+    }
+
+    @GetMapping("/mixon")
+    public Mono<String> getMixOn() {
+        log.info("{}: start", Thread.currentThread().getName());
+        return Mono.fromCallable(() -> "publish")
+                .subscribeOn(Schedulers.elastic())
+                .doOnNext(str -> log.info("{}: first", Thread.currentThread().getName()))
+                .publishOn(Schedulers.elastic())
+                .flatMap(str -> Mono.defer(() -> {
+                    log.info("{}: before get user name", Thread.currentThread().getName());
+                    return userService.getUserName();
+                }))
+                .publishOn(Schedulers.parallel())
+                .zipWith(Mono.defer(() -> Mono.fromCallable(() -> {
+                    log.info("{}: zipwith", Thread.currentThread().getName());
+                    return "zipwith";
+                })))
+                .map(tuple -> {
+                    log.info("{}: tuple map", Thread.currentThread().getName());
+                    return tuple.getT1() + tuple.getT2();
+                });
+    }
 }
