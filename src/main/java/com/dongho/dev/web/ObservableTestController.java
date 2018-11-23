a727ff7272a3744a731c3ac261ec8484680255e8
Dongho Sim <dhsim86@gmail.com>
2018-11-23 12:50:53 +0900

    WebFluxTest: Added sample for Observable / Observer.

---
+++ src/main/java/com/dongho/dev/web/ObservableTestController.java
@@ -0,0 +1,46 @@
+package com.dongho.dev.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+@Slf4j
+@RestController
+@RequestMapping("/observable")
+public class ObservableTestController {
+
+    public static class Observable1 extends Observable {
+
+        public void say() {
+
+            List dataArr = Arrays.asList(1, 2, 3, 4, 5);
+
+            for (Object i : dataArr) {
+                setChanged();
+                notifyObservers(i);
+            }
+
+        }
+
+    }
+
+    @GetMapping("/test1")
+    public Mono<Void> test1() {
+        Observable1 observable = new Observable1();
+
+        Observer observer = (o, data) -> log.info("update: {}", data);
+
+        observable.addObserver(observer);
+        observable.say();
+
+        return Mono.empty();
+    }
+
+}
