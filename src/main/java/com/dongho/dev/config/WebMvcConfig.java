c5377481bcbc554ff9685f33598cf692da5c3fcd
Dongho Sim <dhsim86@gmail.com>
2019-06-03 20:42:00 +0900

    ETag with Cache-Control Header (no-cache / no-store) example.

--- src/main/java/com/dongho/dev/config/WebMvcConfig.java
+++ src/main/java/com/dongho/dev/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.dongho.dev.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,13 +13,21 @@ import java.util.Arrays;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private static final String INDEX_MAPPING_LOCATION_FROM = "/index.html";
+    private static final String INDEX_MAPPLING_LOCATION_TO = "classpath:/public/index.html";
+
     private static final String RESOURCE_MAPPING_LOCATION_FROM = "/**";
     private static final String RESOURCE_MAPPING_LOCATION_TO = "classpath:/public/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(INDEX_MAPPING_LOCATION_FROM)
+                .addResourceLocations(INDEX_MAPPLING_LOCATION_TO)
+                .setCacheControl(CacheControl.noCache().mustRevalidate());
+
         registry.addResourceHandler(RESOURCE_MAPPING_LOCATION_FROM)
-                .addResourceLocations(RESOURCE_MAPPING_LOCATION_TO);
+                .addResourceLocations(RESOURCE_MAPPING_LOCATION_TO)
+                .setCachePeriod(3600 * 24 * 7);
     }
 
     @Bean
--- src/main/java/com/dongho/dev/web/ResourceController.java
+++ src/main/java/com/dongho/dev/web/ResourceController.java
@@ -4,24 +4,21 @@ import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class ResourceController {
 
-    // with no-store, Etag not applied
-    @GetMapping("/no-etag/{path}/**")
-    public String noEtag(@PathVariable String path, HttpServletResponse response) {
+    @GetMapping("/no-store")
+    public String noStore(HttpServletResponse response) {
+        // with no-store, Etag not applied
         response.setHeader(HttpHeaders.CACHE_CONTROL, CacheControl.noStore().mustRevalidate().getHeaderValue());
         return "forward:/index.html";
     }
 
-    // with no-cache, Etag applied
-    @GetMapping("/etag/{path}/**")
-    public String etag(@PathVariable String path, HttpServletResponse response) {
-        response.setHeader(HttpHeaders.CACHE_CONTROL, CacheControl.noCache().mustRevalidate().getHeaderValue());
+    @GetMapping("/no-cache")
+    public String noCache() {
         return "forward:/index.html";
     }
 
