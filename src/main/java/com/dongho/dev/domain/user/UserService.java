67c06e7e26ed77c4061a0eb255c5c5e457e5ed45
Dongho Sim <dhsim86@gmail.com>
2020-04-27 20:25:42 +0900

    method parameter validation
    - https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-validation

--- src/main/java/com/dongho/dev/domain/user/UserService.java
+++ src/main/java/com/dongho/dev/domain/user/UserService.java
@@ -1,4 +1,17 @@
 package com.dongho.dev.domain.user;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Size;
+
+@Slf4j
+@Validated
 public class UserService {
+
+    public String validateTest(@Size(min = 8, max = 10) String code) {
+        log.info("validate Test");
+        return code;
+    }
+
 }
--- src/main/java/com/dongho/dev/web/HelloController.java
+++ src/main/java/com/dongho/dev/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.dongho.dev.web;
 
+import com.dongho.dev.domain.user.UserService;
 import com.dongho.dev.web.protocol.TestValidationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +15,9 @@ import javax.validation.Valid;
 @EnableAutoConfiguration
 public class HelloController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/")
     String home() {
         return "Hello World!";
@@ -21,6 +26,11 @@ public class HelloController {
     @GetMapping("/test-validation")
     public String testValidation(@Valid @ModelAttribute TestValidationRequest request) {
         return request.getTest();
+    }
+
+    @GetMapping("/test-method-validation")
+    public String testMethodValidation() {
+        return userService.validateTest("1");
     }
 
 }
