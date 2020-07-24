edec00bb2f16d76c7e8d5a91276908f2385c3eb3
Dongho Sim <dhsim86@gmail.com>
2020-07-25 01:25:53 +0900

    jenkins webhook test

--- src/main/java/com/dongho/dev/SpringBootStartApplication.java
+++ src/main/java/com/dongho/dev/SpringBootStartApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootStartApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStartApplication.class, args);
 	}
+
 }
